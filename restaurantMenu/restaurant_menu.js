0dbb62406ea431116fe93b57b1ec61ea8d98acc0
Ethem <kesimethem@gmail.com>
2025-09-06 07:57:00 -0400

    restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,17 @@
+const breakfastMenu = ['pancakes', 'eggs benedict', 'oetmeal', 'frittata'];
+const mainCourseMenu = ['steak', 'pasta', 'burger', 'salmon'];
+const dessertMenu = ['cake', 'ice cream', 'pudding', 'fruit salad'];
+
+ const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+ let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
